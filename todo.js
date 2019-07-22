7a0b9d6ee70df6817f93fad1f76e27f38b2e0d88
dnyaneshlb <dnyaneshlb@users.noreply.github.com>
2019-07-22 19:07:29 +0530

    site decoratation
    site decoratation

---
+++ todo.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * add a skill bar : https://codepen.io/tamak/pen/hzEer
+ *                   https://www.w3schools.com/howto/howto_css_skill_bar.asp
+ *                   https://stackoverflow.com/questions/34669269/jquery-skill-bar-effect-starts-when-you-scroll-to-the-section
+ * 
+ * 
+ * 
+ */
